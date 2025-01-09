20be807d6dc039c5b8313fda6088bc617f10b2b0
Rachit Sharma <156568387+Git1709@users.noreply.github.com>
2025-01-09 22:04:35 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,2 +1,5 @@
-export const PORT =5555;
-export const mongodburl= 'mongodb+srv://root:root@cluster0.ilzae.mongodb.net/genshin?retryWrites=true&w=majority&appName=Cluster0'
+// config.js
+
+// Using environment variables for sensitive data
+export const PORT = process.env.PORT || 5555;  // Default to 5555 if not provided
+export const mongodburl = process.env.MONGODB_URL || 'mongodb+srv://root:root@cluster0.ilzae.mongodb.net/genshin?retryWrites=true&w=majority&appName=Cluster0';
